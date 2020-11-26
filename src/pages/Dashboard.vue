e1d084331b149b4d8c5d75c28147e1efbefdf3e3
cksar <cesarhsouzasoad@gmail.com>
2020-11-26 04:32:49 -0300

    Dashboard estruturado

---
+++ src/pages/Dashboard.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <div class="dashboard-header">
+            <img class="circle-top" src="@/assets/images/ellipse-signup-top.png" />
+        </div>
+        <div class="dashboard">
+            
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    
+};
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/Dashboard.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <Header />
+    <div class="app">
+      <Body class="main" />
+    </div>
+  </div>
+</template>
+
+<script>
+import Header from "@/components/Header.vue";
+
+export default {
+  name: "Dashboard",
+  components: {
+    Header,
+  },
+};
+</script>
