30cc1b9c43e379dafec45fad2bc3986d3aa3866b
Keila <keiladaianefernandes24@gmail.com>
2020-12-06 20:29:43 -0300

    resolvendo conflitos

--- src/main.js
+++ src/main.js
@@ -2,19 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import VueHead from 'vue-head'
-import VueToast from 'vue-toast-notification';
-
-import 'vue-toast-notification/dist/theme-sugar.css';
 
 Vue.config.productionTip = false
-
-Vue.use(VueHead)
-Vue.use(VueToast)
 
 new Vue({
   router,
   store,
   render: h => h(App)
 }).$mount('#app')
-
