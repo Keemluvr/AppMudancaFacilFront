4d4d31afca8bb1be155d920afe641955c971f92b
Keila <keiladaianefernandes24@gmail.com>
2020-12-06 20:10:37 -0300

    adicionando redirect e mensagens de alerta

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <link rel="stylesheet" href="iziToast.min.css" />
     <main>
       <transition mode="out-in">
         <router-view />
@@ -11,7 +12,6 @@
 <style>
 @import url("https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap");
 @import url("https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap");
-
 html {
   overflow-x: hidden;
 }
@@ -168,5 +168,9 @@ table {
 .v-enter-active,
 .v-leave-active {
   transition: all 0.3s;
+}
+
+.v-toast__text {
+  color: white !important;
 }
 </style>
