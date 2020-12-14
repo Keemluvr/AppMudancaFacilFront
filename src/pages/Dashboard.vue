028fc7f8c6a8f437982fb9a079bb5958415545c6
cksar <cesarhsouzasoad@gmail.com>
2020-12-14 04:50:51 -0300

    Ajustes na nova dashboard

--- src/pages/Dashboard.vue
+++ src/pages/Dashboard.vue
@@ -1,70 +1,62 @@
 <template>
-  <div>
-    <!--<div class="dashboard-header">
-      <img class="" src="@/assets/images/ellipse-signup-top.png" />
-    </div>-->
-    <div class="dashboard">
-      <div class="dashboard-header">
-        <h1>Meus imóveis</h1>
-        <p>Publicados:</p>
-        <p>Locado:</p>
-      </div>
-      <div />
-      <div class="dashboard-body">
-        <Item class="item" />
-      </div>
-    </div>
+  <div v-if="loading">
+    <Loading
+      :is-full-page="true"
+      :opacity="0.8"
+      loader="dots"
+      color="#3F3D56"
+      :active="true"
+    />
+  </div>
+  <div v-else class="list-items">
+    <Item
+      v-for="(immobile, index) in properties"
+      :key="index"
+      :content="immobile"
+      :errored="errored"
+    />
   </div>
 </template>
 
 <script>
 import Item from "../components/Item.vue";
+import api from "../services/api.js";
+import Loading from "vue-loading-overlay";
+import "vue-loading-overlay/dist/vue-loading.css";
 
 export default {
   name: "Dashboard",
   components: {
     Item,
+    Loading,
+  },
+  data() {
+    return {
+      properties: null,
+      loading: true,
+      errored: false,
+    };
+  },
+  mounted() {
+    api
+      .get("/immobile/owner/5e8e57f08c4cc11fa011bf31")
+      .then((response) => (this.properties = response.data))
+      .catch((error) => {
+        console.log(error);
+        this.errored = true;
+      })
+      .finally(() => (this.loading = false));
   },
 };
 </script>
 
 <style scoped>
-.dashboard-header {
-  position: relative;
-  padding: 2vw;
-  margin: 0 5vw;
+.list-items {
+  margin: 50px 5vw 0 50px;
   height: 100%;
-}
-
-.circle-top {
-  height: auto;
-  width: auto;
-  position: relative;
-  top: 0;
-  left: 200px;
-  z-index: 2000;
-}
-
-h1 {
-  color: #4976ef;
-  font-weight: 400;
-  font-size: 50px;
-  margin: 10vh 0 3vh 0;
-  z-index: 3000;
-  font-family: "Poppins", sans-serif;
-}
-
-.item {
-  margin: 0 5vw;
-  height: 100%;
-}
-
-hr {
-  border: 1px solid rgba(198, 198, 198, 0.5);
-}
-
-.dashboard-body {
-  padding-top: 30px;
-  background-color: rgba(235, 235, 235, 0.644);
+  display: flex;
+  justify-content: space-around;
+  flex-wrap: wrap;
+  gap: 50px;
 }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -24,7 +24,7 @@ const routes = [
     component: Login
   },
   {
-    path: '/dashboard',
+    path: '/meus-imoveis',
     name: 'Dashboard',
     component: Dashboard
   }
