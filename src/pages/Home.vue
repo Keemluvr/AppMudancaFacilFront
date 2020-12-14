32d35edcfae80f6dd8e9d92e8b58e2c3d650062a
cksar <cesarhsouzasoad@gmail.com>
2020-12-14 16:46:11 -0300

    Consulta a API

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,35 +1,57 @@
 <template>
-  <div>
-    <div class="list-cards">
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-    </div>
+  <div v-if="loading">
+    <Loading
+      :is-full-page="true"
+      :opacity="0.8"
+      loader="dots"
+      color="#3F3D56"
+      :active="true"
+    />
+  </div>
+  <div v-else class="list-cards">
+    <Card
+      v-for="(immobile, index) in properties"
+      :key="index"
+      :content="immobile"
+      :errored="errored"
+    />
   </div>
 </template>
 
 <script>
 import Card from "@/components/Card.vue";
+import api from "../services/api.js";
+import Loading from "vue-loading-overlay";
+import "vue-loading-overlay/dist/vue-loading.css";
 
 export default {
   name: "Home",
   components: {
     Card,
+    Loading,
   },
   data() {
     return {
-      immobile: null,
+      properties: null,
+      loading: true,
+      errored: false,
     };
   },
-  mounted() {},
+  mounted() {
+    api
+      .get("/immobile")
+      .then(
+        (response) => (
+          (this.properties = response.data.properties),
+          console.log(this.properties)
+        )
+      )
+      .catch((error) => {
+        console.log(error);
+        this.errored = true;
+      })
+      .finally(() => (this.loading = false));
+  },
 };
 </script>
 
